92f0814cb0080f50e00697b124f54ea1b48f2f80
ShayneHopkins <shayne_hopkins@icloud.com>
2025-12-12 02:08:24 -0800

    finished!

--- bootcamp-milestone-4/src/app/api/blog/[slug]/route.ts
+++ bootcamp-milestone-4/src/app/api/blog/[slug]/route.ts
@@ -4,9 +4,9 @@ import BlogModel, { type Blog } from "../../../../database/blogSchema";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { slug: string } }
+  { params }: { params: Promise<{ slug: string }> }
 ) {
-  const { slug } = params;
+  const { slug } = await params;
 
   try {
     await connectDB();
